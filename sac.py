497d8ceea046d44e286f2173fccf1cf428795fa2
Phil Ball <ball@robots.ox.ac.uk>
2020-02-13 13:15:53 +0000

    more updates

--- sac.py
+++ sac.py
@@ -1,40 +1,63 @@
 from collections import deque
+import itertools
+import random
 
 import torch
 import torch.nn as nn
 
-from .utils import Transition
-
-
-class ReplayPool(object):
-
-    def __init__(self, capacity=1e6):
-        self.capacity = capacity
-        self.transitions = deque(maxlen=capacity)
-        
-    def push(self, transition: Transition):
-        """ Saves a transition """
-        self.memory.append(transition)
-        
-    def sample(self, batch_size: int) -> Transition:
-        transitions = random.sample(self.memory, batch_size)
-        return Transition(*zip(*transitions))
-
-    def get(self, start_idx: int, end_idx: int) -> Transition:
-        transitions = list(itertools.islice(self.memory, start_idx, end_idx))
-        return Transition(*zip(*transitions))
-
-    def get_all(self) -> Transition:
-        return self.get(0, len(self.memory))
-    
-    def __len__(self) -> int:
-        return len(self.memory)
-
+from .utils import Transition, replay_pool
 
 class MLPNetwork(nn.Module):
     
+    def __init__(self, input_dim, output_dim, hidden_size=256):
+        super(MLPNetwork, self).__init__()
+        self.network = nn.Sequential(
+                        nn.Linear(input_dim, hidden_size),
+                        nn.ReLU(),
+                        nn.Linear(hidden_size, hidden_size),
+                        nn.ReLU(),
+                        nn.Linear(hidden_size, hidden_size),
+                        nn.ReLU(),
+                        nn.Linear(hidden_size, output_dim),
+                        )
+    
+    def forward(self, x):
+        return self.network(x)
 
 
-def make_double_q_function():
+def make_double_q_function(state_dim):
+    dual_q = tuple(MLPNetwork(input_dim=state_dim, output_dim=1) for i in range(2))
+    return dual_q
 
 
+class SAC_Agent:
+
+    def __init__(self, seed, state_dim, action_dim, lr=3e-4, gamma=0.99, K_epochs=1, tau=5e-3, batchsize=256, update_interval=1):
+        self.gamma = gamma
+        self.K_epochs = K_epochs
+        # self.state_dim = state_dim
+        # self.action_dim = action_dim
+        self.tau = tau
+        
+        torch.manual_seed(seed)
+
+        self.q_funcs = make_double_q_function(state_dim)
+        self.target_q_funcs = tuple(copy.deepcopy(q_func) for q_func in self.q_funcs)
+
+        self.policy = MLPNetwork(state_dim, action_dim)
+
+        self.q_optimizers = tuple(torch.optim.Adam(q_func.parameters(), lr=lr) for q_func in self.q_funcs)
+        self.policy_optimizer = torch.optim.Adam(self.policy.parameters(), lr=lr)
+
+        self.replay_pool = ReplayPool()
+
+    def update_target(self):
+        """moving average update of target networks"""
+        for target_q_func, q_func in zip(self.target_q_funcs, self.q_funcs):
+            for target_q_param, q_param in zip(target_q_func.parameters(), q_func.parameters()):
+                target_q_param.data.copy_(self.tau * q_param.data + (1 - self.tau) * target_q_param.data)
+
+    def update_q_functions(self, Transitions):
+
+
+    def optimize(self):
--- utils.py
+++ utils.py
@@ -1,3 +1,49 @@
-from collections import namedtuple
+from collections import namedtuple, deque
+import itertools
+import random
+from torch.utils.data import DataLoader, Dataset
 
 Transition = namedtuple('Transition', ('state', 'action', 'reward', 'nextstate'))
+
+
+class ReplayPool:
+
+    def __init__(self, capacity=1e6):
+        self.capacity = int(capacity)
+        self._memory = deque(maxlen=capacity)
+        
+    def push(self, transition: Transition):
+        """ Saves a transition """
+        self._memory.append(transition)
+        
+    def sample(self, batch_size: int) -> Transition:
+        transitions = random.sample(self._memory, batch_size)
+        return Transition(*zip(*transitions))
+
+    def get(self, start_idx: int, end_idx: int) -> Transition:
+        transitions = list(itertools.islice(self._memory, start_idx, end_idx))
+        return Transition(*zip(*transitions))
+
+    def get_all(self) -> Transition:
+        return self.get(0, len(self._memory))
+
+    def __len__(self) -> int:
+        return len(self._memory)
+
+    def clear_pool(self):
+        self._memory.clear()
+
+
+class SACDataSet(Dataset):
+
+    def __init__(self, transitions: Transition):
+        self.states = Transition.state
+        self.actions = Transition.action
+        self.nextstate = Transition.nextstate
+        self.reward = Transition.reward
+
+    def __len__(self):
+        return len(self.states)
+
+    def __getitem__(self, index):
+        return self.states[index], self.actions[index], self.nextstate[index], self.reward[index]
